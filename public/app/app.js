74837941a2619bf60588320086ba6ed684a9f71a
alexandre.l <lienardalexandre@live.fr>
2017-05-30 09:24:37 +0200

    premier commit

---
+++ public/app/app.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const config = [
+    "$stateProvider",
+    "$urlRouteProvider",
+    Config
+]
+
+angular
+    
+    .module('wildNoteApp', [
+        "ui.router"
+    ])
+
+    .config(config)
+
+    // .factory("Hey", function() {
+    //     return {getConseil: () => "utilise les factory pour créer ta ressource !"}
+    // })
+
+function Config($stateProvider, $urlRouteProvider) {
+    const states = [
+        {
+            name: "home",
+            url: "/",
+            component: "home"
+        }
+    ]
+
+    states.forEach((state) => {
+        $stateProvider.state(state)
+    })
+
+    $urlRouterProvider.otherwise('/zergling')
+}
